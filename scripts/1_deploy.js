3c6dfaee35e1d42ddd4bdf3ccc6eed8b7093f962
unknown <ionblockchain@gmail.com>
2023-02-12 14:03:42 +0100

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+async function main() {
+  //Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+
+  //Deploy contract
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log(`Token Deployed to: ${token.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
