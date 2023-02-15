901954f6e0c353ebd9dd55d2eb18f99ad52507a1
unknown <ionblockchain@gmail.com>
2023-02-15 18:49:02 +0100

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
@@ -10,8 +10,15 @@ contract Token {
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor(
         string memory _name,
@@ -30,9 +37,24 @@ contract Token {
     ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         require(_to != address(0));
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 }
