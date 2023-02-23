ec08f25c3505963fe17c6ce1cde8a8c2cde5f723
unknown <ionblockchain@gmail.com>
2023-02-23 12:42:34 +0100

    14. Seed exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,9 +1,7 @@
-const { ethers } = require("hardhat");
-const hre = require("hardhat");
-
 async function main() {
-  console.log(`Preparing deployment... \n`);
-  //Fetch contract to deploy
+  console.log(`Preparing deployment...\n`);
+
+  // Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token");
   const Exchange = await ethers.getContractFactory("Exchange");
 
@@ -11,10 +9,10 @@ async function main() {
   const accounts = await ethers.getSigners();
 
   console.log(
-    `Accounts fetched:\n ${accounts[0].address}\n${accounts[1].address}\n`
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
   );
 
-  //Deploy contracts
+  // Deploy contracts
   const dapp = await Token.deploy("Dapp University", "DAPP", "1000000");
   await dapp.deployed();
   console.log(`DAPP Deployed to: ${dapp.address}`);
@@ -32,7 +30,9 @@ async function main() {
   console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/2_seed-exchange.js
+++ scripts/2_seed-exchange.js
@@ -1,34 +1,45 @@
+const config = require("../src/config.json");
+
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+const wait = (seconds) => {
+  const milliseconds = seconds * 1000;
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
 async function main() {
-  // Fetch accounts from wallet - these are unslocked
+  // Fetch accounts from wallet - these are unlocked
   const accounts = await ethers.getSigners();
+
+  // Fetch network
+  const { chainId } = await ethers.provider.getNetwork();
+  console.log("Using chainId:", chainId);
 
   // Fetch deployed tokens
   const DApp = await ethers.getContractAt(
     "Token",
-    "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+    config[chainId].DApp.address
   );
   console.log(`Dapp Token fetched: ${DApp.address}\n`);
 
   const mETH = await ethers.getContractAt(
     "Token",
-    "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+    config[chainId].mETH.address
   );
-  console.log(`mETH fetched: ${mETH.address}\n`);
+  console.log(`mETH Token fetched: ${mETH.address}\n`);
 
   const mDAI = await ethers.getContractAt(
     "Token",
-    "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+    config[chainId].mDAI.address
   );
-  console.log(`mDAI fetched: ${mDAI.address}\n`);
+  console.log(`mDAI Token fetched: ${mDAI.address}\n`);
 
   // Fetch the deployed exchange
   const exchange = await ethers.getContractAt(
     "Exchange",
-    "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+    config[chainId].exchange.address
   );
   console.log(`Exchange fetched: ${exchange.address}\n`);
 
@@ -37,23 +48,152 @@ async function main() {
   const receiver = accounts[1];
   let amount = tokens(10000);
 
-  // use1 transfers 10000 mETH
+  // user1 transfers 10,000 mETH...
   let transaction, result;
   transaction = await mETH.connect(sender).transfer(receiver.address, amount);
+  console.log(
+    `Transferred ${amount} tokens from ${sender.address} to ${receiver.address}\n`
+  );
 
   // Set up exchange users
   const user1 = accounts[0];
   const user2 = accounts[1];
   amount = tokens(10000);
 
-  // Distribute tokens
+  // user1 approves 10,000 Dapp...
+  transaction = await DApp.connect(user1).approve(exchange.address, amount);
+  await transaction.wait();
+  console.log(`Approved ${amount} tokens from ${user1.address}`);
 
-  // Deposit tokens to exchange
+  // user1 deposits 10,000 DApp...
+  transaction = await exchange
+    .connect(user1)
+    .depositToken(DApp.address, amount);
+  await transaction.wait();
+  console.log(`Deposited ${amount} Ether from ${user1.address}\n`);
 
-  // MAke orders
+  // User 2 Approves mETH
+  transaction = await mETH.connect(user2).approve(exchange.address, amount);
+  await transaction.wait();
+  console.log(`Approved ${amount} tokens from ${user2.address}`);
+
+  // User 2 Deposits mETH
+  transaction = await exchange
+    .connect(user2)
+    .depositToken(mETH.address, amount);
+  await transaction.wait();
+  console.log(`Deposited ${amount} tokens from ${user2.address}\n`);
+
+  /////////////////////////////////////////////////////////////
+  // Seed a Cancelled Order
+  //
+
+  // User 1 makes order to get tokens
+  let orderId;
+  transaction = await exchange
+    .connect(user1)
+    .makeOrder(mETH.address, tokens(100), DApp.address, tokens(5));
+  result = await transaction.wait();
+  console.log(`Made order from ${user1.address}`);
+
+  // User 1 cancels order
+  orderId = result.events[0].args.id;
+  transaction = await exchange.connect(user1).cancelOrder(orderId);
+  result = await transaction.wait();
+  console.log(`Cancelled order from ${user1.address}\n`);
+
+  // Wait 1 second
+  await wait(1);
+
+  /////////////////////////////////////////////////////////////
+  // Seed Filled Orders
+  //
+
+  // User 1 makes order
+  transaction = await exchange
+    .connect(user1)
+    .makeOrder(mETH.address, tokens(100), DApp.address, tokens(10));
+  result = await transaction.wait();
+  console.log(`Made order from ${user1.address}`);
+
+  // User 2 fills order
+  orderId = result.events[0].args.id;
+  transaction = await exchange.connect(user2).fillOrder(orderId);
+  result = await transaction.wait();
+  console.log(`Filled order from ${user1.address}\n`);
+
+  // Wait 1 second
+  await wait(1);
+
+  // User 1 makes another order
+  transaction = await exchange.makeOrder(
+    mETH.address,
+    tokens(50),
+    DApp.address,
+    tokens(15)
+  );
+  result = await transaction.wait();
+  console.log(`Made order from ${user1.address}`);
+
+  // User 2 fills another order
+  orderId = result.events[0].args.id;
+  transaction = await exchange.connect(user2).fillOrder(orderId);
+  result = await transaction.wait();
+  console.log(`Filled order from ${user1.address}\n`);
+
+  // Wait 1 second
+  await wait(1);
+
+  // User 1 makes final order
+  transaction = await exchange
+    .connect(user1)
+    .makeOrder(mETH.address, tokens(200), DApp.address, tokens(20));
+  result = await transaction.wait();
+  console.log(`Made order from ${user1.address}`);
+
+  // User 2 fills final order
+  orderId = result.events[0].args.id;
+  transaction = await exchange.connect(user2).fillOrder(orderId);
+  result = await transaction.wait();
+  console.log(`Filled order from ${user1.address}\n`);
+
+  // Wait 1 second
+  await wait(1);
+
+  /////////////////////////////////////////////////////////////
+  // Seed Open Orders
+  //
+
+  // User 1 makes 10 orders
+  for (let i = 1; i <= 10; i++) {
+    transaction = await exchange
+      .connect(user1)
+      .makeOrder(mETH.address, tokens(10 * i), DApp.address, tokens(10));
+    result = await transaction.wait();
+
+    console.log(`Made order from ${user1.address}`);
+
+    // Wait 1 second
+    await wait(1);
+  }
+
+  // User 2 makes 10 orders
+  for (let i = 1; i <= 10; i++) {
+    transaction = await exchange
+      .connect(user2)
+      .makeOrder(DApp.address, tokens(10), mETH.address, tokens(10 * i));
+    result = await transaction.wait();
+
+    console.log(`Made order from ${user2.address}`);
+
+    // Wait 1 second
+    await wait(1);
+  }
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
