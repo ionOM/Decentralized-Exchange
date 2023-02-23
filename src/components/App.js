54301fb41224d0509c883949c25bd1a4da9aa3ca
unknown <ionblockchain@gmail.com>
2023-02-23 17:30:23 +0100

    save changes

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+
+      <main className="exchange grid">
+        <section className="exchange__section--left grid">
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+        </section>
+        <section className="exchange__section--right grid">
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+        </section>
+      </main>
+
+      {/* Alert */}
+    </div>
+  );
+}
+
+export default App;
