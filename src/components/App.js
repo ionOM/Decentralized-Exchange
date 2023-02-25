2f81b15f34c8d9c5183fd7e34bc8fa7f77a491a0
unknown <ionblockchain@gmail.com>
2023-02-25 12:19:40 +0100

    UI setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import { useEffect } from "react";
+import { ethers } from "ethers";
+import config from "../config.json";
+import TOKEN_ABI from "../abis/Token.json";
 import "../App.css";
 
 function App() {
   const loadBlockchainData = async () => {
-    await window.ethereum.request({ method: `eth_requestAccounts` });
+    const accounts = await window.ethereum.request({
+      method: `eth_requestAccounts`,
+    });
     console.log(accounts[0]);
+
+    // Connect Ethers to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const { chainId } = await provider.getNetwork();
+    console.log(chainId);
+
+    // Token Smart Contract
+    const token = new ethers.Contract(
+      config[chainId].DApp.address,
+      TOKEN_ABI,
+      provider
+    );
+    console.log(token.address);
+    const symbol = await token.symbol();
+    console.log(symbol);
   };
 
   useEffect(() => {
