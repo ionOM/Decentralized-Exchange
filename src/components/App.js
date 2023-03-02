0b5da8a672082078a2a72e953672cfbcf5568b5d
unknown <ionblockchain@gmail.com>
2023-03-02 13:26:56 +0100

    save progress

--- src/components/App.js
+++ src/components/App.js
@@ -22,8 +22,15 @@ function App() {
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch);
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch);
+    // Reload page when network changes
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on("accountsChanged", () => {
+      loadAccount(provider, dispatch);
+    });
 
     // Load token smart contracts
     const DApp = config[chainId].DApp;
